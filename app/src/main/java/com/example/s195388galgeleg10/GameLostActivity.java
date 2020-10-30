2c450177d549bec83ca9e831f45cb37ce93f2e9a
Sarakarkov <s195388@student.dtu.dk>
2020-10-30 12:27:09 +0100

    Implementering af logikken bag won og lost klasserne. Heraf hvordan knapperne skifter skærmbillede

--- app/src/main/java/com/example/s195388galgeleg10/GameLostActivity.java
+++ app/src/main/java/com/example/s195388galgeleg10/GameLostActivity.java
@@ -2,13 +2,48 @@ package com.example.s195388galgeleg10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class GameLostActivity extends AppCompatActivity {
+public class GameLostActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button backToMenu, tryAgain;
+    TextView textViewTheRightWord2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gamelost);
+
+        //Find view by id
+
+        backToMenu = findViewById(R.id.backToMenu);
+        tryAgain = findViewById(R.id.tryAgain);
+        textViewTheRightWord2 = findViewById(R.id.textViewTheRightWord2);
+
+        textViewTheRightWord2.setText("Ordet var: " + getIntent().getExtras().getString("word"));
+
+        //On click
+
+        backToMenu.setOnClickListener(this);
+        tryAgain.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == tryAgain) {
+            Intent i = new Intent(this, GameQuickStartActivity.class);
+            startActivity(i);
+        } else if (v == backToMenu) {
+            Intent i = new Intent(this, GameWelcomeActivity.class);
+            startActivity(i);
+
+        }
     }
 }
--- app/src/main/java/com/example/s195388galgeleg10/GameWonActivity.java
+++ app/src/main/java/com/example/s195388galgeleg10/GameWonActivity.java
@@ -2,13 +2,56 @@ package com.example.s195388galgeleg10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class GameWonActivity extends AppCompatActivity {
+public class GameWonActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button tryAgain, backToMenu, saveHighScore;
+    EditText editTextName;
+    TextView textViewTheRightWord, textViewHowManyTries;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gamewon);
+
+        //View by id
+        tryAgain = findViewById(R.id.tryAgain);
+        backToMenu = findViewById(R.id.backToMenu);
+        saveHighScore = findViewById(R.id.saveHighScore);
+        editTextName = findViewById(R.id.editTextName);
+        textViewTheRightWord = findViewById(R.id.textViewTheRightWord);
+        textViewHowManyTries = findViewById(R.id.textViewHowManyTries);
+
+        textViewHowManyTries.setText("Antal forsøg: " + getIntent().getExtras().getInt("antal forsøg"));
+        textViewTheRightWord.setText("Ordet var: " + getIntent().getExtras().getString("ordet"));
+
+        // // On Click Listener
+
+        tryAgain.setOnClickListener(this);
+        backToMenu.setOnClickListener(this);
+        saveHighScore.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == tryAgain) {
+            Intent i = new Intent(this, GameQuickStartActivity.class);
+            startActivity(i);
+        } else if (v == backToMenu) {
+            Intent i = new Intent(this, GameWelcomeActivity.class);
+            startActivity(i);
+        } else if (v == saveHighScore) {
+            Intent i = new Intent(this, GameHighScoreActivity.class);
+            startActivity(i);
+        }
     }
 }
